69d4caa58cc55b40b957c03d8720931e8caba6a3
Aiden Wallis <aiden@aidenwallis.co.uk>
2021-01-02 17:37:23 +0000

    Update component.tsx

--- src/components/root/component.tsx
+++ src/components/root/component.tsx
@@ -1,22 +1,12 @@
 import * as React from "react";
 import {ThirdPartyEmotesProvider} from "../../contexts/third-party-emotes";
 import {ChatRoot} from "../chat-root";
-
-declare const process: {
-  env: {
-    REACT_APP_TWITCH_LOGIN: string;
-    REACT_APP_TWITCH_ID: string;
-  };
-};
-
-const channelID = process.env.REACT_APP_TWITCH_ID || "87763385";
-const channelLogin =
-  process.env.REACT_APP_TWITCH_LOGIN || "aidenwallis";
+import {SETTINGS} from "../../settings";
 
 export const Root: React.FunctionComponent = () => {
   return (
-    <ThirdPartyEmotesProvider channelId={channelID}>
-      <ChatRoot channelID={channelID} login={channelLogin} />
+    <ThirdPartyEmotesProvider channelId={SETTINGS.TWITCH_ID}>
+      <ChatRoot channelID={SETTINGS.TWITCH_ID} login={SETTINGS.TWITCH_LOGIN} />
     </ThirdPartyEmotesProvider>
   );
 };
