bc7fe19ff4e5614483a4a5b643e96b39de59b4d4
Aiden Wallis <aiden@aidenwallis.co.uk>
2021-01-02 17:26:28 +0000

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,14 @@
+let twitchID = "87763385";
+let twitchLogin = "aidenwallis";
+let [customTwitchID, customTwitchLogin] = window.location.pathname.split('?')[0].substring(1).split('-');
+if (customTwitchID && customTwitchLogin) {
+  twitchID = customTwitchID;
+  twitchLogin = customTwitchLogin;
+}
+
+
 // TODO: This sucks :)
 export const SETTINGS = {
-  TWITCH_ID: "87763385",
-  TWITCH_LOGIN: "aidenwallis",
+  TWITCH_ID: twitchID,
+  TWITCH_LOGIN: twitchLogin,
 };
