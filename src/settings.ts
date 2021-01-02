aac82d089d139debf3aa0f9a221c68cda61f1bcf
Aiden Wallis <aiden@aidenwallis.co.uk>
2021-01-02 17:27:24 +0000

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
 let twitchID = "87763385";
 let twitchLogin = "aidenwallis";
-let [customTwitchID, customTwitchLogin] = window.location.pathname.split('?')[0].substring(1).split('-');
+const [customTwitchID, customTwitchLogin] = window.location.pathname.split('?')[0].substring(1).split('-');
 if (customTwitchID && customTwitchLogin) {
   twitchID = customTwitchID;
   twitchLogin = customTwitchLogin;
