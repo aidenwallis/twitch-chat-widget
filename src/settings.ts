33f0c91799405ae90eae4c6d61b2ac322b8aa70d
Aiden Wallis <aiden@aidenwallis.co.uk>
2021-01-02 17:34:55 +0000

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,13 +1,12 @@
 let twitchID = "87763385";
 let twitchLogin = "aidenwallis";
 const [customTwitchID, customTwitchLogin] = window.location.pathname.split('?')[0].substring(1).split('-');
+console.log({customTwitchID, customTwitchLogin});
 if (customTwitchID && customTwitchLogin) {
   console.log('Found custom Twitch ID & login...');
   twitchID = customTwitchID;
   twitchLogin = customTwitchLogin;
 }
-
-console.log({customTwitchID, customTwitchLogin});
 
 
 // TODO: This sucks :)
