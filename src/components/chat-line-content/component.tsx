d40562d086acb15e3d1c45709e14a259974db147
Aiden <aiden@aidenwallis.co.uk>
2022-10-03 22:55:48 +0100

    Update component.tsx (#5)

--- src/components/chat-line-content/component.tsx
+++ src/components/chat-line-content/component.tsx
@@ -4,12 +4,10 @@ import {ChatMessage} from "../../models";
 import classes from "./styles.module.scss";
 
 interface Props {
-  color: string;
   message: ChatMessage;
 }
 
 export const ChatLineContent: React.FunctionComponent<Props> = ({
-  color,
   message,
 }: Props) => {
   const content = useMessageContent(message);
